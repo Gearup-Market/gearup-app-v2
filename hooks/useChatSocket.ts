0575f5c15a61246796a3a5b0ba8ed178e292479c
uezekiel-mufin <geniuswealthb@gmail.com>
2024-10-14 17:17:26 +0100

    Changes

--- hooks/useChatSocket.ts
+++ hooks/useChatSocket.ts
@@ -16,6 +16,7 @@ export const useChatSocket = (chatId?: string) => {
     // Establish socket connection
     const socketInstance = io(SOCKET_SERVER_URL,{
       withCredentials: true,
+      transports: ['websocket'],
     } );
 
     setSocket(socketInstance);
