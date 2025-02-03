9fa4b1c48b5d5810a1f2bb63ae756a9b9544dfce
nano235 <revivaladolor@yahoo.com>
2025-02-03 20:16:42 +0100

    fixed messages and listing app breaking and updating categories linking

--- mock/categories.mock.ts
+++ mock/categories.mock.ts
@@ -4,41 +4,41 @@ export const categories: Categories[] = [
 	{
 		title: "cameras",
 		image: "/images/camera.png",
-		url: "#",
+		url: "/listings?category=cameras"
 	},
 	{
 		title: "lenses",
 		image: "/images/lense.png",
-		url: "#",
+		url: "/listings?category=lenses"
 	},
 	{
 		title: "audio",
 		image: "/images/audio.png",
-		url: "#",
+		url: "/listings?category=audio"
 	},
 	{
 		title: "lighting",
 		image: "/images/lighting.png",
-		url: "#",
+		url: "/listings?category=lighting"
 	},
 	{
 		title: "gimbals",
 		image: "/images/gimbal.png",
-		url: "#",
+		url: "/listings?category=gimbals"
 	},
 	{
 		title: "studio spaces",
 		image: "/images/studio.png",
-		url: "#",
+		url: "/listings?category=studio"
 	},
 	{
 		title: "drones",
 		image: "/images/drone.png",
-		url: "#",
+		url: "/listings?category=drones"
 	},
 	{
 		title: "grip",
 		image: "/images/grip.png",
-		url: "#",
-	},
+		url: "/listings?category=grip"
+	}
 ];
