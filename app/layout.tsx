6ce8a8c52294659ef211d8337fb23c7a3e91f0b4
nano235 <revivaladolor@yahoo.com>
2025-03-05 20:23:17 +0100

    updated gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 gtag('config', 'G-WDQ59KDMS6');
             `}
 					</Script>
+					<Script
+						id="google-tag-manager"
+						strategy="afterInteractive"
+						dangerouslySetInnerHTML={{
+							__html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K99BQVGP');
+        `
+						}}
+					/>
+
 					<script
 						async
 						src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
@@ -90,6 +104,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className={sourceSans.className}>
 				<Providers>{children}</Providers>
 			</body>
+			<noscript>
+				<iframe
+					src={`https://www.googletagmanager.com/ns.html?id=GTM-K99BQVGP`}
+					height="0"
+					width="0"
+					style={{ display: "none", visibility: "hidden" }}
+				></iframe>
+			</noscript>
 		</html>
 	);
 }
