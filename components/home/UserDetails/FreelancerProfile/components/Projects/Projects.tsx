4fe4984c5a6fa896c25a755e25737ceeaccfed4b
uezekiel-mufin <geniuswealthb@gmail.com>
2025-05-06 13:22:55 +0100

    Fix responsiveness

--- components/home/UserDetails/FreelancerProfile/components/Projects/Projects.tsx
+++ components/home/UserDetails/FreelancerProfile/components/Projects/Projects.tsx
@@ -51,8 +51,10 @@ const projects = [
 ];
 
 const responsive = {
-  superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
-  desktop: { breakpoint: { max: 3000, min: 1024 }, items: 4 },
+  desktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 3,
+  },
   tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
   mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
 };
@@ -77,8 +79,6 @@ const Projects = () => {
         slidesToSlide={1}
         keyBoardControl={true}
         transitionDuration={500}
-        containerClass="carousel-container"
-        itemClass="carousel-item-padding-40-px"
         centerMode={true}
         focusOnSelect={true}
       >
